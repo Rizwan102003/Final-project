0f615d42b7164ca2e68fcfa5c1ba5d2c5e2e035f
Rizwan <skmdrizwan2003@gmail.com>
2025-10-12 17:05:02 +0530

    model1

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, File, UploadFile
+import requests
+from io import BytesIO
+
+app = FastAPI()
+
+MODEL_URL = "Samudraman i lost yo url"
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    
+    files = {"file": ("image.jpg", image_bytes, "image/jpeg")}
+    response = requests.post(MODEL_URL, files=files)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": f"Model API returned {response.status_code}"}
